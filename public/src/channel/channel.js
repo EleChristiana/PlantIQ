e690168c1c9899f3c64ccee83844fee62fb04677
Calia21 <elechristacoal@gmail.com>
2024-11-06 13:00:55 -0800

    adjusted css again

--- public/src/channel/channel.js
+++ public/src/channel/channel.js
@@ -3,33 +3,33 @@
 
 
 
-const videoSection = document.querySelector('.video-section')
+// const videoSection = document.querySelector('.video-section')
 
 
-fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=16&playlistId=UUSbyncU597LMwb3HhnAI_4w&key=AIzaSyDJN3NgxzVTy6kBcjRj6GUnPsIckK1653o').then(res => res.json())
-    .then(data => {
-      console.log(data);
-        data.items.forEach(el => {
+// fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=16&playlistId=UUSbyncU597LMwb3HhnAI_4w&key=AIzaSyDJN3NgxzVTy6kBcjRj6GUnPsIckK1653o').then(res => res.json())
+//     .then(data => {
+//       console.log(data);
+//         data.items.forEach(el => {
 
-            videoSection.innerHTML +=
-                `<div class="yt-video-container">
-                <a target = "_blank" href ="https://www.youtube.com/watch?v=${el.snippet.resourceId.videoId}"  class = "yt-video">
-        <img src = "${el.snippet.thumbnails.maxres.url}" class = "yt-img" width = 350px />
-        </a>
-        <p class="yt-title">${el.snippet.title}</p>
-        </div>`
-        })
+//             videoSection.innerHTML +=
+//                 `<div class="yt-video-container">
+//                 <a target = "_blank" href ="https://www.youtube.com/watch?v=${el.snippet.resourceId.videoId}"  class = "yt-video">
+//         <img src = "${el.snippet.thumbnails.maxres.url}" class = "yt-img" width = 350px />
+//         </a>
+//         <p class="yt-title">${el.snippet.title}</p>
+//         </div>`
+//         })
 
 
-    })
+//     })
 
-    function myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-        x.className += " responsive";
-      } else {
-        x.className = "topnav";
-      }
-    }
+//     function myFunction() {
+//       var x = document.getElementById("myTopnav");
+//       if (x.className === "topnav") {
+//         x.className += " responsive";
+//       } else {
+//         x.className = "topnav";
+//       }
+//     }
    
 
